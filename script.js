40ba2d79c3bfc18c4d468fd0f92c9c51d718ce1e
RichVelo <richrar@gmail.com>
2022-09-20 17:44:27 +0100

    added Js to hide rating screen and show thanks

--- script.js
+++ script.js
@@ -9,3 +9,14 @@ numbers.forEach((number) => {
 function makeNumbersDisplay(number) {
     return '<div class="numberButton">' + number + '</div>'
 }
+
+
+let submitButton = document.querySelector('#submit')
+let rateHide = document.querySelector('#rate')
+let thanksDisplay = document.querySelector('#thank-you')
+submitButton.addEventListener('click', () => {
+    rateHide.classList.remove('rate')
+	rateHide.classList.add('hidden')
+    thanksDisplay.classList.remove('hidden')
+    thanksDisplay.classList.add('thank-you')
+})
